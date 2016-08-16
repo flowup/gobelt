123b0da74ec737b18716c9397b80cbb445d226bb
Gelidus <gelidus@flowup.eu>
2016-08-16 15:16:07 +0200

    feat(generator): added Generate boilerplate

--- generator.go
+++ generator.go
@@ -1,5 +1,27 @@
 package gobelt
 
-type Generator interface {
-  Generate(args []string) error
+import (
+  "github.com/flowup/gogen"
+  "path/filepath"
+)
+
+type GenerateCallback func(build *gogen.Build, file string, dir string) error
+
+func Generate(files []string, cb GenerateCallback) error {
+  for _, file := range files {
+    // get the dir
+    path := filepath.Dir(file)
+
+    // get the build
+    build, err := gogen.ParseFile(file)
+    if err != nil {
+      return err
+    }
+
+    if err = cb(build, file, path); err != nil {
+      return err
+    }
+  }
+
+  return nil
 }
--- observablegen/observable.go
+++ observablegen/observable.go
@@ -5,6 +5,7 @@ import (
   "path/filepath"
 
   "github.com/flowup/gogen"
+  "github.com/flowup/gobelt"
 )
 
 // TemplateData is a data structure for the observable template
@@ -19,32 +20,23 @@ type __T__ struct {}
 // Generate parses all given files by args and generates observable
 // structures for structures annotated by @observable build tag
 func Generate(args []string) error {
-  for _, arg := range args {
-    // get the dir
-    path := filepath.Dir(arg)
-
-    // get the build
-    build, err := gogen.ParseFile(arg)
-    if err != nil {
-      return err
-    }
-
+  return gobelt.Generate(args, func(build *gogen.Build, filePath, dir string) error {
     // retrieve the file from the build
-    file := build.Files[arg]
+    file := build.Files[filePath]
 
     data := TemplateData{
       Package: file.Package(),
     }
 
-    for stName, _ := range file.Structs().Filter("@observable") {
-      _, err := os.Create(filepath.Join(path, stName + ".observable.gen.go"))
+    for stName := range file.Structs().Filter("@observable") {
+      _, err := os.Create(filepath.Join(dir, stName + ".observable.gen.go"))
       if err != nil {
         return err
       }
 
       data.ModelName = stName
     }
-  }
 
-  return nil
+    return nil
+  })
 }
