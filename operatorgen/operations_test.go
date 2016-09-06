c09b5b70f4a271782afe2051bf12003a193a91ec
Tomáš Hanák <tomashanak17@gmail.com>
2016-09-06 14:13:04 +0200

    feat(Operations): Extended variaty of operations (Find, FindOne, ForEach, Reverse, Sort, Push, Pop) plus added tests.

--- operatorgen/operations_test.go
+++ operatorgen/operations_test.go
@@ -9,10 +9,14 @@ import (
 
 // Expecteds results from tests
 var (
-	MapResultArray1    = TTypeSequence{&TType{3}, &TType{6}, &TType{9}, &TType{12}}
-	MapResultArray2    = TTypeSequence{&TType{1}, &TType{4}, &TType{9}, &TType{16}}
+	MapResultArray1    = TTypeSequence{&TType{3}, &TType{6}, &TType{15}, &TType{9}, &TType{12}}
+	MapResultArray2    = TTypeSequence{&TType{1}, &TType{4}, &TType{25}, &TType{9}, &TType{16}}
 	FilterResultArray1 = TTypeSequence{&TType{2}, &TType{4}}
-	FilterResultArray2 = TTypeSequence{&TType{1}, &TType{3}}
+	FilterResultArray2 = TTypeSequence{&TType{1}, &TType{5}, &TType{3}}
+	ReverseResultArray = TTypeSequence{&TType{4}, &TType{3}, &TType{5}, &TType{2}, &TType{1}}
+	SortedResultArray  = TTypeSequence{&TType{1}, &TType{2}, &TType{3}, &TType{4}, &TType{5}}
+	PushResultArray    = TTypeSequence{&TType{1}, &TType{2}, &TType{5}, &TType{3}, &TType{4}, &TType{100}}
+	PopResultArray     = TTypeSequence{&TType{1}, &TType{2}, &TType{5}, &TType{3}}
 )
 
 // TemplateTestSuite is testing suite
@@ -23,7 +27,7 @@ type TemplateTestSuite struct {
 
 // SetupSuite is setting up array for all the tests
 func (s *TemplateTestSuite) SetupSuite() {
-	s.array = TTypeSequence{&TType{1}, &TType{2}, &TType{3}, &TType{4}}
+	s.array = TTypeSequence{&TType{1}, &TType{2}, &TType{5}, &TType{3}, &TType{4}}
 
 }
 
@@ -75,7 +79,74 @@ func (s *TemplateTestSuite) TestReduce() {
 	tmp := s.array.Reduce(func(a, b *TType) *TType {
 		return &TType{a.code * b.code}
 	}, &TType{1})
-	assert.Equal(s.T(), &TType{24}, tmp)
+	assert.Equal(s.T(), &TType{120}, tmp)
+}
+
+// TestReduce is testing functionality of FindOne fuction
+func (s *TemplateTestSuite) TestFindOne() {
+
+	tmp := s.array.FindOne(func(num *TType) bool {
+		if num.code%2 == 0 {
+			return true
+		}
+		return false
+	})
+	assert.Equal(s.T(), &TType{2}, tmp)
+
+	tmp = s.array.FindOne(func(num *TType) bool {
+		if num.code%2 == 0 {
+			return true
+		}
+		return false
+	})
+	assert.NotEqual(s.T(), FilterResultArray1, tmp)
+}
+
+// TestReverse is testing Reversing function
+func (s *TemplateTestSuite) TestReverse() {
+
+	tmp := s.array.Reverse()
+
+	assert.Equal(s.T(), ReverseResultArray, tmp)
+}
+
+// TestSort is testing functionality of Sort function
+func (s *TemplateTestSuite) TestSort() {
+
+	tmp := s.array.Sort(func(a, b *TType) bool {
+		if a.code > b.code {
+			return true
+		}
+		return false
+	})
+	assert.Equal(s.T(), SortedResultArray, tmp)
+}
+
+// TestReverse is testing Reversing function
+func (s *TemplateTestSuite) TestPush() {
+
+	tmp := s.array.Push(&TType{100})
+	assert.Equal(s.T(), PushResultArray, tmp)
+
+}
+
+// TestPop is testing Pop function
+func (s *TemplateTestSuite) TestPop() {
+
+	tmp := s.array.Pop()
+	assert.Equal(s.T(), PopResultArray, tmp)
+
+}
+
+// TestReverse is testing Reversing function
+func (s *TemplateTestSuite) TestForEach() {
+	sum := 0
+	s.array.ForEach(func(num *TType) {
+		sum += num.code
+
+	})
+	assert.Equal(s.T(), 15, sum)
+
 }
 
 // Starting the test
--- operatorgen/template.go
+++ operatorgen/template.go
@@ -6,11 +6,17 @@ type TTypeSequence []*TType
 // TTypeMapFunc is function using abstract TType
 type TTypeMapFunc func(t *TType) *TType
 
+// TTypeForEachFunc is function using abstract TType
+type TTypeForEachFunc func(t *TType)
+
 // TTypeFilterFunc is func for filtering that is taking TType and return boolean
 type TTypeFilterFunc func(t *TType) bool
 
 // TTypeReduceFunc is taking two TTypes and returns TType based on operation applied between those two
 type TTypeReduceFunc func(first *TType, second *TType) *TType
+
+// TTypeSortFunc is taking two TTypes and returns TType based on operation applied between those two
+type TTypeSortFunc func(first *TType, second *TType) bool
 
 // Map is function that is making operation defined by mapper function above all the elements of an array
 func (seq TTypeSequence) Map(mapper TTypeMapFunc) TTypeSequence {
@@ -46,4 +52,69 @@ func (seq TTypeSequence) Reduce(reducer TTypeReduceFunc, init *TType) *TType {
 	}
 
 	return curr
+}
+
+// Find is alias for Filter function, for implementation details see Filter
+func (seq TTypeSequence) Find(filter TTypeFilterFunc) TTypeSequence {
+	res := seq.Filter(filter)
+	return res
+}
+
+// FindOne is finding first appearance of element that satysfies predice in sequence
+func (seq TTypeSequence) FindOne(predicate TTypeFilterFunc) *TType {
+	for _, val := range seq {
+		if predicate(val) {
+			return val
+		}
+	}
+	return nil
+}
+
+// Reverse is reversing sequence and is returning that new alocated reversed sequence
+func (seq TTypeSequence) Reverse() TTypeSequence {
+	res := make([]*TType, len(seq))
+
+	for i, j := 0, len(seq)-1; i <= j; i, j = i+1, j-1 {
+		res[i], res[j] = seq[j], seq[i]
+	}
+	return res
+}
+
+// ForEach is function that is iterating over the sequence
+func (seq TTypeSequence) ForEach(function TTypeForEachFunc) {
+	for _, val := range seq {
+		function(val)
+	}
+}
+
+// Sort is sorting sequence based on function sortedBy
+func (seq TTypeSequence) Sort(sortedBy TTypeSortFunc) TTypeSequence {
+	size := len(seq)
+	res := seq
+	if size < 2 {
+		return res
+	}
+	for i := 0; i < size; i++ {
+		for j := size - 1; j >= i+1; j-- {
+			if sortedBy(res[j-1], res[j]) {
+				res[j], res[j-1] = res[j-1], res[j]
+			}
+		}
+	}
+	return res
+}
+
+// Push func
+func (seq TTypeSequence) Push(elem *TType) TTypeSequence {
+	res := seq
+	res = append(res, elem)
+
+	return res
+}
+
+// Pop func
+func (seq TTypeSequence) Pop() TTypeSequence {
+	res := seq[:len(seq)-1]
+
+	return res
 }
